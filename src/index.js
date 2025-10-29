35115d5719e7b6d9f17c6de196a48eb8243a7765
bitturohit <rohit97750@gmail.com>
2025-10-29 20:24:32 +0530

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+import App from "./App";
+
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
